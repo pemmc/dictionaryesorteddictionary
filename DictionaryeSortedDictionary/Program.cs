52e9c47c0271a4f92f0a360d2f2b4602220e9f73
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-26 18:16:36 -0300

    214. Exercício proposto sobre Dictionary

--- DictionaryeSortedDictionary/Program.cs
+++ DictionaryeSortedDictionary/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using System.IO;
+
 namespace DictionaryeSortedDictionary
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /*
             Dictionary<string, string> cookies = new Dictionary<string, string>();
 
 
@@ -49,6 +52,57 @@ namespace DictionaryeSortedDictionary
                 Console.WriteLine(item.Key + ": " + item.Value);
 
             }
+            */
+
+            //Windows
+            //string path = @"C:\Users\apagar\Projects\DictionaryeSortedDictionary\file.txt";
+
+            //macOS note
+            //string path = "/Users/pauloeduardo/Projects/DictionaryeSortedDictionary/file.txt";
+
+            //macOS PC
+            string path = "/Users/nxgames/Projects/DictionaryeSortedDictionary/file.txt";
+
+            Console.WriteLine("Full path: " + path);
+
+            //214. Exercício proposto sobre Dictionary
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
